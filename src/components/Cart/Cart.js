b6cb12fcb7e923e81d981c9e63f5fd0629b241b0
mukitcoder <mukitbd71@gmail.com>
2021-09-26 19:51:38 +0600

    cartIcon

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Cart.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserTie } from "@fortawesome/free-solid-svg-icons";
 
 const Cart = (props) => {
-  console.log(props.cart);
+  const speakers = <FontAwesomeIcon icon={faUserTie} />;
   const { cart } = props || {};
   let total = 0;
   for (const guest of cart) {
@@ -10,8 +12,8 @@ const Cart = (props) => {
   }
   return (
     <div className='text-white cart-area'>
-      <h4 className="text-warning">Total Guest Hired: {cart.length}</h4>
-      <h5>Total fees: $ {total} </h5>
+      <h4 className="text-warning"> {speakers} Total Guest Hired: {cart.length}</h4>
+      <h5 className="my-4">Total fees: $ {total} </h5>
       <div className='text-white mt-4'>
         {cart.map((guest) => (<div className="cart-add-area rounded-pill">
             <p className="px-2 mx-2"><img className="mx-3 rounded my-auto" src={guest.img} alt="" /> {guest.name}</p>
--- src/components/Guest/Guest.js
+++ src/components/Guest/Guest.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Guest.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoins } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookSquare,
+  faInstagramSquare,
+  faTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Guest = (props) => {
-  console.log(props);
-  const coin = <FontAwesomeIcon icon={faCoins} />
+  const coin = <FontAwesomeIcon icon={faCoins} />;
+  const fb = <FontAwesomeIcon icon={faFacebookSquare} />;
+  const insta = <FontAwesomeIcon icon={faInstagramSquare} />;
+  const twitter = <FontAwesomeIcon icon={faTwitterSquare} />;
   const { name, img, age, occupation, fee, country } = props.guest;
   return (
     <div className="col guest">
@@ -17,16 +24,20 @@ const Guest = (props) => {
           <p className="card-text">Occupation: {occupation}</p>
           <p className="card-text">Fee: ${fee}</p>
           <p className="card-text">Country: {country}</p>
-            <div className="mx-auto w-100">
-            <button 
-            onClick={() => props.handleAddToCart(props.guest)}
-            className="regular-btn btn btn-warning opacity-75"
-          >{coin} Hire Now</button>
-            </div>
+          <div className="mx-auto w-100">
+            <button
+              onClick={() => props.handleAddToCart(props.guest)}
+              className="regular-btn btn btn-warning opacity-75"
+            >
+              {coin} Hire Now
+            </button>
+          </div>
         </div>
 
-        <div className="card-footer">
-          <small className="text-muted">Last updated 3 mins ago</small>
+        <div className="card-footer fs-3 d-flex mx-auto p-0">
+          <p className="text-white social-icon">{fb}</p>
+          <p className="text-white px-5 social-icon">{twitter}</p>
+          <p className="text-white social-icon">{insta}</p>
         </div>
       </div>
     </div>
